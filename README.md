8288a057cf16e4f85d8283514d74650e02a68c7b
Sandeep25dev <sandeepkumar250903@gmail.com>
2024-10-28 08:42:01 +0000

    completed the signin and signup route

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # backend-based-todo-app
+
+### The front-end of the application is not designed yet, So till then you can test the app/program on Postman
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const { UserModel, TodoModel } = require("./db");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET, auth } = require("./auth");
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://sandeepkumar250903:sandip10@cluster0.ptlwf.mongodb.net/todo-app-database"
+);
+
+app.use(express.json());
 
 app.get("/", function (req, res) {
   res.send("Home Page");
@@ -8,26 +16,55 @@ app.get("/", function (req, res) {
 
 app.post("/signup", async function (req, res) {
   const email = req.body.email;
-  const name = req.body.name;
   const password = req.body.password;
+  const name = req.body.name;
 
-  await UserModel.insert({
+  await UserModel.create({
     email: email,
-    name: name,
     password: password,
+    name: name,
   });
 
   res.json({
-    message: "You're signed up",
+    message: "You are signed up",
   });
 });
 
-app.post("/login", function (req, res) {});
+app.post("/signin", async function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+  try {
+    // UserModel is reffered to the users collection in  our database
+    const response = await UserModel.findOne({
+      email: email,
+      password: password,
+    });
 
-app.post("/todo", function (req, res) {});
+    if (response) {
+      const token = jwt.sign(
+        {
+          id: response._id.toString(),
+        },
+        JWT_SECRET
+      );
 
-app.get("/todos", function (req, res) {});
+      res.json({
+        token: token,
+      });
+    } else {
+      res.status(403).json({
+        message: "Incorrect Credentials",
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
 
-app.listen(3000, function () {
-  console.log("Server is live on PORT 3000");
+app.post("/todo", auth, function (req, res) {});
+
+app.get("/todos", auth, function (req, res) {});
+
+app.listen(3003, function () {
+  console.log("Server is live on PORT 3003");
 });
